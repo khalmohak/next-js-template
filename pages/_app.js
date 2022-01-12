47258982b27946d6914f499eb3c3e4d470266f0e
Mohak Gupta <45048164+khalmohak@users.noreply.github.com>
2022-01-12 13:59:47 +0530

    Next JS project in JavaScript with Chakra UI and Redux set up

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { wrapper } from "../redux/store"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+  )
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp);
